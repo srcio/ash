2fbcc8e196bcef2b7324b397781c1a61df240b8d
dp <dp.io@outlook.com>
2022-09-05 17:41:37 +0800

    add ali registry

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker buildx build --push --platform=linux/amd64,linux/arm64 . -t srcio/ash -f Dockerfile
+docker buildx build --push --platform=linux/amd64,linux/arm64 . -t srcio/ash -t registry.cn-hangzhou.aliyuncs.com/srcio/ash -f Dockerfile
